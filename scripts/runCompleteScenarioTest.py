6dd60744c3ec741e131653389e44031c29d8fe89
stefzke <spfluege@gmail.com>
2017-07-03 09:29:30 +0200

    added eclipse settings and the one button script for luminosity analysis

---
+++ scripts/runCompleteScenarioTest.py
@@ -0,0 +1,386 @@
+# ok this whole procedure is extremely tedious and costly... 
+# so dont be scared when you see all this...
+
+import random, os, sys, re, time, glob, errno
+lib_path = os.path.abspath('argparse-1.2.1/build/lib')
+sys.path.append(lib_path)
+import argparse
+import himster
+from decimal import *
+import subprocess
+import json
+
+import general, simulation
+
+base_out_dir = "/data/work/himspecf/pflueger/realistic_scenarios"
+os.environ["DATA_DIR"] = base_out_dir
+low_index = 1
+high_index = 100
+num_events = 100000
+
+lmd_fit_path='/home/pflueger/LuminosityFit'
+lmd_fit_script_path = '/home/pflueger/LuminosityFit/scripts'
+lmd_fit_bin_path = '/home/pflueger/LuminosityFit/build/bin'
+
+#this is kind of bad... but I had to quick fix it... im sorry
+gen_lumi_per_event={1.5:171.91969551926493, 4.06:36.382231019899820, 8.9:16.294680278930652, 15.0:10.578901730437558}
+
+class Scenario:   
+    def __init__(self, dir_path_):
+        self.dir_path = dir_path_
+        self.filtered_dir_path = ''
+        self.acc_and_res_dir_path = ''
+        self.ip_rec_file = ''
+    
+        self.counter = 1
+        self.last_counter = 0
+    
+        self.simulation_info_lists = []
+
+def wasSimulationSuccessful(directory, glob_pattern):
+    required_files_percentage=0.8
+    # return values:
+    # 0: everything is fine
+    # >0: its not finished processing, just keep waiting
+    # <0: something went wrong...
+    return_value=0 
+    
+    found_files = glob.glob(directory + '/' + glob_pattern)
+    good_files=[]
+    bad_files=[]
+    for file in found_files:
+        if os.stat(file).st_size > 20000:
+            good_files.append(file)
+        else:
+            bad_files.append(file)
+
+    m = re.search('\/(\d+?)-(\d+?)_.+?cut', directory)
+    num_sim_files=int(m.group(2))-int(m.group(1)) + 1
+    
+    if 1.0*len(good_files) < required_files_percentage*num_sim_files:
+        print 'WARNING: more than 20% of sim files missing... Something went wrong here...'
+        print directory
+        # (time.time()-os.path.getmtime('path'))/60/60/24        
+        if himster.getNumJobsOnHimster() > 0:
+            return_value=1
+        else:
+            return_value=-1
+    
+    return return_value
+    
+# ------------------------------------------------------------------------------------------
+# ok we do it in such a way we have a step counter for each directory and two stacks
+# we try to process 
+active_scenario_stack = []
+waiting_scenario_stack = []
+dead_scenario_stack = []
+
+
+def simulateDataOnHimster(sim_info_list, lab_momentum, ip_rec_file=''):
+    dir_path = sim_info_list[0]
+    type = sim_info_list[1]
+    counter = sim_info_list[2]
+    last_counter = sim_info_list[3]
+
+    print 'running simulation of type ' + str(type) + ' and path (' + dir_path + ') at counter=' + str(counter) + '/' + str(last_counter)
+
+    data_keywords = []
+    data_pattern = ''
+    merge_keywords = ['merge_data', 'binning_300']
+    if 'v' in type:
+        data_keywords = ['uncut', 'bunches', 'binning_300']
+        data_pattern = 'lmd_vertex_data_*.root'
+    elif 'a' in type:
+        data_keywords = ['xy_m_cut_real', 'bunches', 'binning_300']
+        data_pattern = 'lmd_data_*.root'
+    else:
+        data_keywords = ['xy_m_cut_real', 'bunches', 'binning_300']
+        data_pattern = 'lmd_res_data_*.root'
+    
+    # 1. simulate data
+    if counter == 1:
+        os.chdir(lmd_fit_script_path)
+        if 'er' in type:
+            found_dirs = []
+            status_code=1
+            if dir_path != '':
+                temp_dir_searcher = general.DirectorySearcher(['box', 'xy_m_cut'])
+                temp_dir_searcher.searchListOfDirectories(dir_path, 'Lumi_TrksQA_*.root')
+                found_dirs = temp_dir_searcher.getListOfDirectories()
+
+            if found_dirs:
+                status_code=wasSimulationSuccessful(found_dirs[0], 'Lumi_TrksQA_*.root')
+
+            elif last_counter < 1:
+                # then lets simulate
+                file_content = open(ip_rec_file)   
+                ip_rec_data = json.load(file_content)
+
+                sim_params = simulation.SimulationParameters()
+                sim_params.ip_params.ip_offset_x = float('{0:.3f}'.format(round(float(ip_rec_data["ip_x"]), 3)))  # in cm
+                sim_params.ip_params.ip_offset_y = float('{0:.3f}'.format(round(float(ip_rec_data["ip_y"]), 3)))
+                sim_params.ip_params.ip_offset_z = float('{0:.3f}'.format(round(float(ip_rec_data["ip_z"]), 3)))
+            
+                sim_params.num_events = num_events
+                sim_params.lab_momentum = lab_momentum
+                sim_params.sim_type = 'box'
+                sim_params.low_index = low_index
+                sim_params.high_index = high_index
+                sim_params.use_xy_cut = True
+                sim_params.use_m_cut = True
+                sim_params.reco_ip_offset = [sim_params.ip_params.ip_offset_x, sim_params.ip_params.ip_offset_y, sim_params.ip_params.ip_offset_z]
+                 
+                generator_filename_base = simulation.generateGeneratorBaseFilename(sim_params)
+                dirname = simulation.generateDirectory(sim_params, generator_filename_base)
+                dirname_filter_suffix = simulation.generateFilterSuffix(sim_params)
+
+                dirname_full = dirname + '/' + dirname_filter_suffix
+                sim_info_list[0] = os.getenv('DATA_DIR') + '/' + dirname_full
+            
+                try:
+                    os.makedirs(base_out_dir+'/'+dirname)
+                except OSError as exception:
+                    if exception.errno != errno.EEXIST:
+                        print 'warning: thats strange that directory already exists...'
+            
+                temp_dir_searcher = general.DirectorySearcher(['box', 'xy_m_cut'])
+                temp_dir_searcher.searchListOfDirectories(base_out_dir+'/'+dirname, 'Lumi_TrksQA_*.root')
+                found_dirs = temp_dir_searcher.getListOfDirectories()
+            
+                if not found_dirs:
+                    bashcommand = 'python runSimulations.py --low_index ' + str(low_index) + ' --high_index ' + str(high_index) \
+                        + ' --use_ip_offset ' + str(sim_params.ip_params.ip_offset_x) + ' ' + str(sim_params.ip_params.ip_offset_y) + ' ' + str(sim_params.ip_params.ip_offset_z) \
+                        + ' ' + str(sim_params.ip_params.ip_spread_x) + ' ' + str(sim_params.ip_params.ip_spread_y) + ' ' + str(sim_params.ip_params.ip_spread_z) \
+                        + ' --reco_ip_offset ' + str(sim_params.ip_params.ip_offset_x) +' ' + str(sim_params.ip_params.ip_offset_y) + ' ' + str(sim_params.ip_params.ip_offset_z) \
+                        + ' --track_search_algo CA --use_xy_cut --use_m_cut ' + str(num_events) + ' ' + str(lab_momentum) + ' box'
+                    returnvalue = subprocess.call(bashcommand.split())
+                    last_counter = last_counter + 1
+                else:
+                    print 'found directory with simulated files, skipping...'
+                    dir_path = found_dirs[0]
+                    scen.dir_path = found_dirs[0]
+                    counter = 2
+                    last_counter = 1
+            
+            if status_code == 0:
+                print 'found simulation files, skipping...'
+                counter = 2
+                last_counter = 1
+            elif status_code > 0:
+                print 'still waiting for himster simulation jobs for ' + type + ' data to complete...'
+            else:
+                #ok something went wrong there, exit this scenario and push on bad scen stack
+                last_counter=-1
+                
+        elif 'a' in type:
+            status_code=1
+            temp_dir_searcher = general.DirectorySearcher(['dpm_elastic', 'xy_m_cut'])
+            temp_dir_searcher.searchListOfDirectories(dir_path, 'Lumi_TrksQA_*.root')
+            found_dirs = temp_dir_searcher.getListOfDirectories()
+            if found_dirs:
+                status_code=wasSimulationSuccessful(found_dirs[0], 'Lumi_TrksQA_*.root')
+            elif last_counter < counter:
+                # then lets simulate
+                file_content = open(ip_rec_file)   
+                ip_rec_data = json.load(file_content)
+                
+                ip_params = simulation.IPParams()
+                ip_params.ip_offset_x = float('{0:.3f}'.format(round(float(ip_rec_data["ip_x"]), 3)))  # in cm
+                ip_params.ip_offset_y = float('{0:.3f}'.format(round(float(ip_rec_data["ip_y"]), 3)))
+                ip_params.ip_offset_z = float('{0:.3f}'.format(round(float(ip_rec_data["ip_z"]), 3)))
+                    
+            
+                bashcommand = 'python runSimulations.py --low_index ' + str(low_index) + ' --high_index ' + str(high_index) \
+                    + ' --reco_ip_offset ' + str(ip_params.ip_offset_x) + ' ' + str(ip_params.ip_offset_y) + ' ' + str(ip_params.ip_offset_z) \
+                    + ' --output_dir ' + dir_path.replace(base_out_dir + '/', '') \
+                    + ' --track_search_algo CA --use_xy_cut --use_m_cut ' + str(num_events) + ' ' + str(lab_momentum) + ' dpm_elastic'
+                returnvalue = subprocess.call(bashcommand.split())
+                last_counter = last_counter + 1
+            
+
+            if status_code == 0:
+                print 'found simulation files, skipping...'
+                counter = 2
+                last_counter = 1
+            elif status_code > 0:
+                print 'still waiting for himster simulation jobs for ' + type + ' data to complete...'
+            else:
+                #ok something went wrong there, exit this scenario and push on bad scen stack
+                last_counter=-1
+        else:
+            # just skip simulation for vertex data... we always have that...
+            print 'skipping simulation step...'
+            counter = 2
+            last_counter = 1 
+    # 2. create data (that means bunch data, create data objects)
+    if counter == 2:
+        # check if data objects already exists and skip!
+        temp_dir_searcher = general.DirectorySearcher(data_keywords)
+        temp_dir_searcher.searchListOfDirectories(dir_path, data_pattern)
+        found_dirs = temp_dir_searcher.getListOfDirectories()
+        if found_dirs:
+            print 'skipping bunching and data object creation...'
+            counter = 3
+            last_counter = 2
+        elif last_counter < counter:
+            os.chdir(lmd_fit_script_path)
+            # 1a bunch data
+            bashcommand = 'python makeMultipleFileListBunches.py --files_per_bunch 10 ' + dir_path
+            returnvalue = subprocess.call(bashcommand.split())
+            # 1b create data
+            if 'a' in type:
+                ref_mom, gen_lumi = min(gen_lumi_per_event.items(), key=lambda(k,v):abs(k-lab_momentum))
+		bashcommand = 'python createMultipleLmdData.py --elastic_cross_section ' + str(gen_lumi) + ' --dir_pattern ' + data_keywords[0] + ' ' + str(lab_momentum) + ' ' + type + ' ' + dir_path + ' ../dataconfig_xy.json'
+            else:    
+		bashcommand = 'python createMultipleLmdData.py --dir_pattern ' + data_keywords[0] + ' ' + str(lab_momentum) + ' ' + type + ' ' + dir_path + ' ../dataconfig_xy.json'
+            returnvalue = subprocess.call(bashcommand.split())
+            last_counter = last_counter + 1
+        else:
+            print 'still waiting for himster data creation jobs for ' + type + ' data to complete...'
+    
+    # 3. merge data
+    if counter == 3:
+        # check first if merged data already exists and skip it!
+        temp_dir_searcher = general.DirectorySearcher(merge_keywords)
+        temp_dir_searcher.searchListOfDirectories(dir_path, data_pattern)
+        found_dirs = temp_dir_searcher.getListOfDirectories()
+        if not found_dirs:
+            os.chdir(lmd_fit_script_path)
+            # 1c merge vertex data
+            bashcommand = 'python mergeMultipleLmdData.py --dir_pattern ' + data_keywords[0] + ' ' + type + ' ' + dir_path
+            returnvalue = subprocess.call(bashcommand.split())
+        counter = -1
+    
+    sim_info_list[2] = counter
+    sim_info_list[3] = last_counter
+    
+    return sim_info_list
+        
+        
+        
+        
+def lumiDetermination(scen):
+    dir_path = scen.dir_path
+
+    counter = scen.counter
+    last_counter = scen.last_counter
+    
+    print 'processing scenario ' + dir_path + ' at step ' + str(counter)
+    
+    m = re.search('(\d*?\.\d*?)GeV', dir_path)
+    momentum = float(m.group(1))
+    print momentum
+    
+    finished = False
+    # 1. create vertex data (that means bunch data, create data objects and merge)
+    if counter == 1:
+        if len(scen.simulation_info_lists) == 0:
+            scen.simulation_info_lists.append([dir_path, 'v', 1, 0])
+        
+        scen.simulation_info_lists[0] = simulateDataOnHimster(scen.simulation_info_lists[0], momentum)
+        if scen.simulation_info_lists[0][2] == -1:
+            counter = counter + 1
+            last_counter = last_counter + 1
+            scen.simulation_info_lists = []
+        elif scen.simulation_info_lists[0][3] == -1:
+            dead_scenario_stack.append(scen)
+            return
+
+    if counter == 2:
+        # check if ip was already determined
+        temp_dir_searcher = general.DirectorySearcher(['merge_data', 'binning_300'])
+        temp_dir_searcher.searchListOfDirectories(dir_path, 'reco_ip.json')
+        found_dirs = temp_dir_searcher.getListOfDirectories()
+        if not found_dirs:
+            # 2. determine offset on the vertex data sample
+            os.chdir(lmd_fit_bin_path)
+            temp_dir_searcher = general.DirectorySearcher(['merge_data', 'binning_300'])
+            temp_dir_searcher.searchListOfDirectories(dir_path, 'lmd_vertex_data_*of1.root')
+            found_dirs = temp_dir_searcher.getListOfDirectories()
+            bashcommand = './determineBeamOffset -p ' + found_dirs[0] + ' -c ' + '../../vertex_fitconfig.json'
+            returnvalue = subprocess.call(bashcommand.split())
+            scen.ip_rec_file = found_dirs[0] + '/reco_ip.json'
+        else:
+            scen.ip_rec_file = found_dirs[0] + '/reco_ip.json'
+        counter = counter + 1
+        last_counter = last_counter + 1 
+            
+    if counter == 3:
+        # 3a. track filter the dpm data using the ip values and create ang dist objects
+        # (that again means bunch -> create -> merge)
+        # 3b. generate acceptance and resolution with these reconstructed ip values
+        # (that means simulation + bunching + creating data objects + merging....)
+        if len(scen.simulation_info_lists) == 0:
+            scen.filtered_dir_path = os.path.dirname(scen.dir_path)
+            scen.simulation_info_lists.append([scen.filtered_dir_path, 'a', 1, 0])
+            if not os.path.exists(scen.filtered_dir_path):
+                os.makedirs(scen.filtered_dir_path)
+            scen.simulation_info_lists.append(['', 'er', 1, 0])
+        
+        temp_sim_lists = scen.simulation_info_lists
+        scen.simulation_info_lists = []
+        for sim in temp_sim_lists:
+            temp_sim = simulateDataOnHimster(sim, momentum, scen.ip_rec_file)
+            if 'er' in temp_sim[1]:
+                scen.acc_and_res_dir_path = temp_sim[0]
+            if temp_sim[2] != -1:
+               scen.simulation_info_lists.append(temp_sim)
+            if temp_sim[3] == -1:
+                dead_scenario_stack.append(scen)
+                return
+        
+        if len(scen.simulation_info_lists) == 0:
+                counter = counter + 1
+                last_counter = last_counter + 1
+            
+    if counter == 4:    
+        # 4. runLmdFit!
+        temp_dir_searcher = general.DirectorySearcher(['merge_data', 'binning_300'])
+        temp_dir_searcher.searchListOfDirectories(scen.filtered_dir_path, 'lmd_fitted_data*.root')
+        found_dirs = temp_dir_searcher.getListOfDirectories()
+        if not found_dirs:
+            os.chdir(lmd_fit_script_path)
+            print 'running lmdfit!'
+            bashcommand = 'python doMultipleLuminosityFits.py --forced_box_gen_data ' + scen.acc_and_res_dir_path + ' ' + scen.filtered_dir_path + ' xy_m_cut_real ' + lmd_fit_path+'/fitconfig.json'
+            returnvalue = subprocess.call(bashcommand.split())        
+        
+        print 'this scenario is fully processed!!!'
+        finished = True
+
+    # if we are in an intermediate step then push on the waiting stack and increase step counter
+    if not finished:
+        scen.counter = counter
+        scen.last_counter = last_counter
+        waiting_scenario_stack.append(scen)
+        
+        
+        
+# first lets try to find all directories and their status/step
+dir_searcher = general.DirectorySearcher(['dpm_elastic', 'uncut'])
+
+dir_searcher.searchListOfDirectories(base_out_dir, 'Lumi_TrksQA_*.root')
+dirs = dir_searcher.getListOfDirectories()
+
+print dirs
+
+# at first assign each scenario the first step and push on the active stack
+for dir in dirs:
+    scen = Scenario(dir)
+    active_scenario_stack.append(scen)
+
+
+# now just keep processing the active_stack
+while len(active_scenario_stack) > 0 or len(waiting_scenario_stack) > 0:
+    for scen in active_scenario_stack:
+        lumiDetermination(scen)
+
+    active_scenario_stack = []
+    # if all scenarios are currently processed just wait a bit and check again
+    if len(waiting_scenario_stack) > 0:
+        print 'currently waiting for 10min to process scenarios again'
+        time.sleep(600)  # wait for 5min
+        active_scenario_stack = waiting_scenario_stack    
+        waiting_scenario_stack = []
+
+# ------------------------------------------------------------------------------------------
+
---
+++ scripts/simulation.py
@@ -0,0 +1,197 @@
+import os, sys, re
+from decimal import *
+
+class IPParams:
+  ip_offset_x = Decimal('0.0')  # in cm
+  ip_offset_y = Decimal('0.0')  # in cm
+  ip_offset_z = Decimal('0.0')  # in cm
+  ip_spread_x = Decimal('0.08')  # in cm
+  ip_spread_y = Decimal('0.08')  # in cm
+  ip_spread_z = Decimal('0.35')  # in cm
+  
+  beam_tilt_x = Decimal('0.0')  # in rad
+  beam_tilt_y = Decimal('0.0')  # in rad
+  beam_divergence_x = Decimal('0.0')  # in rad
+  beam_divergence_y = Decimal('0.0')  # in rad
+
+  def __repr__(self):
+    return 'IP center: [' + str(self.ip_offset_x) + ',' + str(self.ip_offset_y) + ',' + str(self.ip_offset_z) \
+           + '] IP spread: [' + str(self.ip_spread_x) + ',' + str(self.ip_spread_y) + ',' + str(self.ip_spread_z) \
+           + '] Tilt: [' + str(self.beam_tilt_x) + ',' + str(self.beam_tilt_y) \
+           + '] Divergence: [' + str(self.beam_divergence_x) + ',' + str(self.beam_divergence_y) + ']\n'
+  def __str__(self):
+    return self.__repr__()
+
+class SimulationParameters:
+    ip_params = IPParams()
+    
+    num_events = 0
+    lab_momentum = 0.0
+    sim_type = ''
+    force_level = 0
+    gen_data_dirname = ''
+    low_index=0
+    high_index=0
+    gen_data_dir=os.getenv('GEN_DATA')
+    output_dir=''
+    use_xy_cut=False
+    use_m_cut=False
+    track_search_algo='CA'
+    reco_ip_offset=[]
+
+def generateSimulationParameterPropertyFile(output_dirname, sim_params):
+  config_fileurl = output_dirname + '/sim_params.config'
+  if not os.path.isfile(config_fileurl):
+    f = open(config_fileurl, 'w')
+    f.write('ip_mean_x=' + str(sim_params.ip_params.ip_offset_x) + '\n')
+    f.write('ip_mean_y=' + str(sim_params.ip_params.ip_offset_y) + '\n')
+    f.write('ip_mean_z=' + str(sim_params.ip_params.ip_offset_z) + '\n')
+  
+    f.write('ip_standard_deviation_x=' + str(sim_params.ip_params.ip_spread_x) + '\n')
+    f.write('ip_standard_deviation_y=' + str(sim_params.ip_params.ip_spread_y) + '\n')
+    f.write('ip_standard_deviation_z=' + str(sim_params.ip_params.ip_spread_z) + '\n')
+  
+    f.write('beam_tilt_x=' + str(sim_params.ip_params.beam_tilt_x) + '\n')
+    f.write('beam_tilt_y=' + str(sim_params.ip_params.beam_tilt_y) + '\n')
+    f.write('beam_divergence_x=' + str(sim_params.ip_params.beam_divergence_x) + '\n')
+    f.write('beam_divergence_y=' + str(sim_params.ip_params.beam_divergence_y) + '\n')
+
+    f.close()
+
+def generateGeneratorBaseFilename(sim_params):
+  # generator file prefix
+  if sim_params.gen_data_dirname != '':
+    generator_filename_base = sim_params.gen_data_dirname
+  else:
+    if sim_params.sim_type != 'noise':
+      generator_filename_base = getGeneratedDataDirectory(sim_params)
+    else:
+      if sim_params.low_index == -1 or sim_params.high_index == -1:
+        print 'Please specify job array boundaries for noise simulations via --low_index and --high_index since the data will be generated here directly!'
+        sys.exit(1)
+      else:
+        low_index_used = sim_params.low_index
+        high_index_used = sim_params.high_index
+      generator_filename_base = str(sim_params.num_events) + '_noise_plab_' + str(sim_params.lab_momentum) + 'GeV'
+
+  print 'using generator dir: ' + generator_filename_base
+  return generator_filename_base
+
+def generateDirectory(sim_params, generator_filename_base):
+  print 'preparing simulations in index range ' + str(sim_params.low_index) + ' - ' + str(sim_params.high_index)
+
+  if sim_params.output_dir == '':
+    # generate output directory name
+    # lets generate a folder structure based on the input
+    dirname = 'plab_' + str(sim_params.lab_momentum) + 'GeV'
+  
+    gen_part = re.sub('_plab_.*GeV', '', generator_filename_base) 
+    gen_part = re.sub('^\d*_', '', gen_part)
+  
+    dirname += '/' + gen_part
+  
+    dirname += '/ip_offset_XYZDXDYDZ_'+str(sim_params.ip_params.ip_offset_x)+'_'+str(sim_params.ip_params.ip_offset_y)+'_'\
+        +str(sim_params.ip_params.ip_offset_z)+'_'+str(sim_params.ip_params.ip_spread_x)+'_'\
+        +str(sim_params.ip_params.ip_spread_y)+'_'+str(sim_params.ip_params.ip_spread_z)
+
+    dirname += '/beam_grad_XYDXDY_'+str(sim_params.ip_params.beam_tilt_x)+'_'+str(sim_params.ip_params.beam_tilt_y)+'_'\
+        +str(sim_params.ip_params.beam_divergence_x)+'_'+str(sim_params.ip_params.beam_divergence_y)
+
+    dirname += '/' + str(sim_params.num_events)
+  else:
+    dirname = sim_params.output_dir
+    
+  return dirname
+  
+def generateFilterSuffix(sim_params):
+  dirname_filter_suffix = str(sim_params.low_index) + '-' + str(sim_params.high_index) + '_'
+  if sim_params.use_xy_cut:
+    dirname_filter_suffix += 'xy_'
+  if sim_params.use_m_cut:
+    dirname_filter_suffix += 'm_'
+  if not sim_params.use_xy_cut and not sim_params.use_m_cut:
+    dirname_filter_suffix += 'un'
+  dirname_filter_suffix += 'cut'
+  if sim_params.use_xy_cut:
+    if sim_params.reco_ip_offset[0] != 0.0 or sim_params.reco_ip_offset[1] != 0.0 or sim_params.reco_ip_offset[2] != 0.0:
+      dirname_filter_suffix += '_real'
+
+  return dirname_filter_suffix
+
+  
+def getGeneratedDataDirectory(sim_params):  
+  found_dir_dict = dict()
+  
+  gen_data_dir_path = sim_params.gen_data_dir
+
+  while not os.path.isdir(gen_data_dir_path):
+    gen_data_dir_path = raw_input('Please enter valid generator base path: ')
+  
+  for gen_dir_name in os.listdir(gen_data_dir_path):
+    match_data_type_and_momentum = re.search(sim_params.sim_type + '_plab_' + str(sim_params.lab_momentum) + 'GeV', gen_dir_name)
+    if not match_data_type_and_momentum:
+      continue
+    
+    match_num_events = re.search('^(\d*)_', gen_dir_name)
+    if match_num_events:
+      if sim_params.num_events <= int(match_num_events.group(1)):
+        found_dir_dict[gen_dir_name] = int(match_num_events.group(1))
+   
+  # filter for possible index ranges
+  found_dir_dict_temp = found_dir_dict.copy()
+  found_dir_dict.clear()
+  for (dirname, num_events) in found_dir_dict_temp.items():
+    found_index_range = checkIndexRangeForGeneratedDataDirectory(gen_data_dir_path + '/' + dirname)
+    used_start_index = sim_params.low_index
+    used_end_index = sim_params.high_index
+    if(sim_params.low_index == -1):
+      used_start_index = found_index_range[0]
+    if(sim_params.high_index == -1):
+      used_end_index = found_index_range[1]
+    if found_index_range[0] <= used_start_index and used_end_index <= found_index_range[1]:
+      found_dir_dict[dirname] = [num_events, used_start_index, used_end_index]
+  
+  return_result = ''
+  
+  if len(found_dir_dict) == 0:
+    print 'Found no suitable generated data directory in ' + gen_data_dir_path + ' .'\
+          + 'Please specify another path or generate the needed data first!'
+    sys.exit(1)
+  if len(found_dir_dict) > 1:
+    print 'Found more than one suitable generated data directory in ' + gen_data_dir_path + ' .'\
+          + 'List of possibilities is shown below:'
+    print found_dir_dict
+    return_result = ''
+    while return_result not in found_dir_dict.keys():
+      return_result = raw_input('Please enter the dirname of one of the found generator directory names (sorted by most possible user request): ')
+  else:
+    return_result = found_dir_dict.keys()[0]
+
+  sim_params.low_index = found_dir_dict[return_result][1]
+  sim_params.high_index = found_dir_dict[return_result][2]
+
+  return return_result
+  
+
+def checkIndexRangeForGeneratedDataDirectory(dir_path):
+  # check for the index range in the specified generator folder
+  first = 1
+  lowest_index = -1
+  highest_index = -1
+
+  dircontent = os.listdir(dir_path)
+
+  for file in dircontent:
+    result = re.search('_(\d*).root$', file)
+    if result:
+      if first:
+        lowest_index = int(result.group(1))
+        highest_index = int(result.group(1))
+        first = 0
+      else:
+        if int(result.group(1)) < lowest_index:
+          lowest_index = int(result.group(1))
+        elif int(result.group(1)) > highest_index:
+          highest_index = int(result.group(1))
+
+  return [lowest_index, highest_index]
