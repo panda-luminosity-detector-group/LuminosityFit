0d901cac3448c23eb20c5b8861b5158885016939
JFrech <jfrech@ep1.ruhr-uni-bochum.de>
2021-03-27 12:47:37 +0100

    fix: runKoaReco.py now starts

--- python/runKoaReco.py
+++ python/runKoaReco.py
@@ -1,20 +1,21 @@
 import argparse
-import json
 import os
 
-import alignment
-import general
-import reconstruction
 from lumifit.alignment import AlignmentParameters
 from lumifit.general import load_params_from_file
-from lumifit.reconstruction import RecoParameters
+from lumifit.reconstruction import ReconstructionParameters
 
-parser.add_argument('force_level', metavar='force_level', type=int, default=0,
-                    help='"force level 0: if directories exist with data\n"
-                          "files no new simulation is started\n"
-                          "force level 1: will do full reconstruction even if "
-                          "this data already exists, but not geant simulation\n"
-                          "force level 2: resimulation of everything!"')
+parser = argparse.ArgumentParser(
+    description="Call for important variables" " Detector.",
+    formatter_class=argparse.RawTextHelpFormatter,
+)
+
+parser.add_argument('--force_level', metavar='force_level', type=int, default=0,
+                    help="force level 0: if directories exist with data\n"
+                          + "files no new simulation is started\n"
+                          + "force level 1: will do full reconstruction even if "
+                          + "this data already exists, but not geant simulation\n"
+                          + "force level 2: resimulation of everything!")
 parser.add_argument('dirname', metavar='dirname', type=str, nargs=1,
                     help='This directory for the outputfiles.')
 parser.add_argument('pathname', metavar='pathname', type=str, nargs=1,
@@ -22,17 +23,21 @@ parser.add_argument('pathname', metavar='pathname', type=str, nargs=1,
 parser.add_argument('macropath', metavar='macropath', type=str, nargs=1,
                     help='This the path to the macros')
 
+args = parser.parse_args()
 
+debug = 1
+filename_index = 1
+dirname = os.path.abspath(args.dirname[0])
+pathname = os.path.abspath(args.pathname[0])
+macropath = os.path.abspath(args.macropath[0])
 
-if not os.path.isdir(args.pathname):
- os.system("mkdir -p args.pathname")
-
-reco_params = RecoParameters(**load_params_from_file(path_mc_data + "/.."))
-ali_params = AlignmentParameters(**load_params_from_file(path_mc_data + "/.."))
+reco_params = RecoParameters(**load_params_from_file(path_mc_data + "/recoparams.config"))
+ali_params = AlignmentParameters()
 
 verbosityLevel: int = 0
-start_evt: int = $((${num_evts} * ${filename_index}))
-workpathname = "lokalscratch/" + ${SLURM_JOB_ID} + "/" + dirname
+start_evt: int = reco_params.num_events_per_sample * filename_index 
+#workpathname = "lokalscratch/" + ${SLURM_JOB_ID} + "/" + dirname
+workpathname = f"{os.getcwd()}/tmpOutput"
 gen_filepath = workpathname + "/gen_mc.root"
 scriptpath = os.getcwd()
 
@@ -42,8 +47,8 @@ misspl = True
 
 #use cuts during trk seacrh with "CA". Should be 'false' if sensors missaligned!
 trkcut = True
- if ali_params.alignment_matrices_path = "":
-   trkcut = False
+#if ali_params.alignment_matrices_path = "":
+#   trkcut = False
 
 #merge hits on sensors from different sides. true=yes
 mergedHits = True
@@ -58,16 +63,26 @@ prefilter = False
 if reco_params.use_xy_cut :
   prefilter = True
 
-check_stage_success "workpathname + "/Koala_MC_${start_evt}.root""
-if 0 == "$?" or  1 == "force_level":
-  os.system("root -l -b -q 'runKoaPixel2Reco.C('{reco_params.num_events_per_sample}','${start_evt}',"'workpathname'", "'{ali_params.alignment_matrices_path}'", "'{ali_params.misalignment_matrices_path}'", '{ali_params.use_point_transform_misalignment}', 'verbositylvl')'")
+def check_stage_sucess() -> bool:
+        return False
+
+#check_stage_success "workpathname + "/Koala_MC_${start_evt}.root""
+if not check_stage_sucess() or force_level ==  1:
+  os.system(f"root -l -b -q 'KoaPixel2Reco.C({reco_params.num_events_per_sample},"
+          + f"{start_evt},"
+          + f"\"workpathname\","
+         #+ f"\"{ali_params.alignment_matrices_path}\", "
+         #+ f"\"{ali_params.misalignment_matrices_path}\", {ali_params.use_point_transform_misalignment},"
+          + f"{verbositylvl})'"
+          )
 
 
-check_stage_success "workpathname + "/Koala_reco_${start_evt}.root""
-if 0 == "$?" or  1 == "force_level":
-  os.system("root -l -b -q 'runLumiPixel2Reco.C('{reco_params.num_events_per_sample}','${start_evt}',"'workpathname'",'verbositylvl')'")
+#check_stage_success "workpathname + "/Koala_reco_${start_evt}.root""
+#if not check_stage_sucess() or force_level ==  1:
+#  os.system(f"root -l -b -q 'LumiPixel2Reco.C({reco_params.num_events_per_sample},{start_evt},"
+#          + f"\"workpathname\",verbositylvl)'")
   # copy Lumi_recoMerged_ for module aligner
-  os.system("cp workpathname/Koala_reco_${start_evt}.root pathname/Koala_reco_${start_evt}.root")
+#  os.system(f"cp workpathname/Koala_reco_{start_evt}.root pathname/Koala_reco_{start_evt}.root")
 
 os.system("cd scriptpath")
 os.system("./runLmdPairFinder.sh")
