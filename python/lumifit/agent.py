a91b19a9a93f8b26bcc1f5326f86f485937e7fc3
Roman Klasen <roklasen@uni-mainz.de>
2022-03-21 14:10:58 +0100

    updated docstring, auto format

--- python/lumifit/agent.py
+++ python/lumifit/agent.py
@@ -1,27 +1,14 @@
 #!/usr/bin/env python3
-
 """
-Simple program that runs commands given in a named pipe and returns their 
-output and return code in two other pipes.
+Simple program that runs commands given as a json-fomratted object
+in a named pipe and returns their outputs and return code in the same way.
 
-TODO: use json objects for orders, enviromentment variables and so on,
-and return json objects with stdout/stderr outputs and return code
-
-json command structure:
-order type (regular, meta (i.e. for the agent itself))
-order value
-enviroment variables
-shell=(true/false)
-
-return json object structure:
-stdout value
-stderr value
-return code
-runtime maybe? not sure if needed
+the structure of the order and return object is governd by the SlurmOrder Class.
 """
 
 import attr, json, os, shlex, subprocess, sys
-from typing import Any, Dict
+from typing import Dict
+
 
 @attr.s(hash=True)
 class SlurmOrder:
@@ -52,22 +39,28 @@ class SlurmOrder:
 
     def toJson(self) -> str:
         return json.dumps(self.__dict__)
-    
+
+
 def preparePipes(universalPipeName) -> None:
     if not os.path.exists(universalPipeName):
         os.mkfifo(universalPipeName)
 
+
 def deletePipes(universalPipeName) -> None:
     if os.path.exists(universalPipeName):
         os.unlink(universalPipeName)
+
 
 # continouosly read from pipe and execute, write output and return codes back
 def pipeIO(universalPipeName) -> None:
     while True:
         # read entire pipe contents and try to deserialize json from it (close pipe!)
         with open(universalPipeName, 'r', encoding='utf-8') as universalPipe:
-            payload = json.load(universalPipe)          # this handles the entire pipe (encoding, newlines and all)
-            thisOrder = SlurmOrder.fromDict(payload)    # python json's are actually dicts
+            payload = json.load(
+                universalPipe
+            )  # this handles the entire pipe (encoding, newlines and all)
+            thisOrder = SlurmOrder.fromDict(
+                payload)  # python json's are actually dicts
 
         # execute command as ordered
         returnOrder = execute(thisOrder)
@@ -75,27 +68,44 @@ def pipeIO(universalPipeName) -> None:
         with open(universalPipeName, 'w', encoding='utf-8') as universalPipe:
             json.dump(returnOrder.toJson(), universalPipe)
 
-def execute(thisOrder) -> SlurmOrder:
 
-    if(thisOrder.orderType == 'meta'):
-        if(thisOrder.cmd == 'exit'):
+def execute(thisOrder: SlurmOrder) -> SlurmOrder:
+
+    if (thisOrder.orderType == 'meta'):
+        if (thisOrder.cmd == 'exit'):
             bail()
 
-    elif(thisOrder.orderType == 'regular'):
+    elif (thisOrder.orderType == 'regular'):
         # this returns a CompletedProcess
-        process = subprocess.run(thisOrder.cmd, shell=thisOrder.runShell, env=thisOrder.env, capture_output=True, encoding='utf-8')
+
+        if not thisOrder.runShell:
+            cmds = shlex.split(thisOrder.cmd)
+            process = subprocess.run(cmds,
+                                     env=thisOrder.env,
+                                     capture_output=True,
+                                     shell=False,
+                                     encoding='utf-8')
+
+        else:
+            process = subprocess.run(thisOrder.cmd,
+                                     env=thisOrder.env,
+                                     capture_output=True,
+                                     shell=True,
+                                     encoding='utf-8')
 
         # write stdout, stderr and return code to returned SlurmOrder
         thisOrder.returnCode = process.returncode
         thisOrder.stdout = process.stdout
         thisOrder.stderr = process.stderr
         return thisOrder
-    
+
+
 def bail() -> None:
     print(f'Received "exit" command. Exiting now.')
     deletePipes(universalPipe)
     print('Have a nice day.')
     sys.exit(0)
+
 
 def SlurmOrderExamples():
     slurmOrder = SlurmOrder()
@@ -113,6 +123,7 @@ def SlurmOrderExamples():
     print(f'now, backwards from Json:\n{SlurmOrder.fromJson(jsonString)}\n')
     sys.exit(0)
 
+
 if __name__ == '__main__':
     welcome = """
     Agent starting and forking to background.
@@ -129,12 +140,12 @@ if __name__ == '__main__':
         if pid > 0:
             # Exit parent process
             sys.exit(0)
-    
+
     except OSError:
         print(f"fork failed: {e.errno} ({e.strerror})")
         sys.exit(1)
-    
+
     universalPipe = '/tmp/lmdfit'
     preparePipes(universalPipe)
-    
-    pipeIO(universalPipe)    
+
+    pipeIO(universalPipe)
