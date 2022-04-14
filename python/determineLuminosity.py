d9678f173ccfebb21d9793a0f090cb80cd0e812e
Roman Klasen <roklasen@uni-mainz.de>
2022-04-14 11:44:57 +0200

    fixed wrong TrksQA file name, externalized config class

--- python/determineLuminosity.py
+++ python/determineLuminosity.py
@@ -15,8 +15,7 @@ import socket
 from lumifit import alignment
 from lumifit import himster
 from lumifit import reconstruction
-
-from enum import Enum
+from lumifit.scenario import Scenario, ExperimentType
 
 from lumifit.alignment import AlignmentParameters
 import lumifit.general as general
@@ -33,45 +32,6 @@ from lumifit.agent import Client
 from lumifit.cluster import ClusterJobManager
 from lumifit.gsi_virgo import create_virgo_job_handler
 from lumifit.himster import create_himster_job_handler
-
-
-class ExperimentType(Enum):
-    LUMI = "LUMI"
-    KOALA = "KOALA"
-
-
-class Scenario:
-    def __init__(self, dir_path_, experiment_type: ExperimentType):
-        self.momentum = 0.0
-
-        self.dir_path = dir_path_
-        self.filtered_dir_path = ""
-        self.acc_and_res_dir_path = ""
-        self.rec_ip_info = {}
-        self.elastic_pbarp_integrated_cross_secion_in_mb = None
-        self.use_m_cut = True
-        self.use_xy_cut = True
-        self.use_ip_determination = True
-        self.Lumi = True
-        if experiment_type == ExperimentType.LUMI:
-            self.phi_min_in_rad = 0
-            self.phi_max_in_rad = 2 * math.pi
-            self.Sim = "./runLmdSimReco.py"
-            self.Reco = "./runLmdReco.py"
-        else:
-            self.phi_min_in_rad = 0.9 * math.pi
-            self.phi_max_in_rad = 1.3 * math.pi
-            self.Sim = "./runKoalaSimReco.py"
-            self.Reco = "./runKoalaReco.py"
-
-        self.alignment_parameters: AlignmentParameters = AlignmentParameters()
-
-        self.state = 1
-        self.last_state = 0
-
-        self.simulation_info_lists = []
-
-        self.is_broken = False
 
 
 def wasSimulationSuccessful(
@@ -636,7 +596,7 @@ parser.add_argument(
     metavar="base_output_data_dir",
     type=str,
     default=os.getenv("LMDFIT_DATA_DIR"),
-    help="Base directory for output files created by this" " script.\n",
+    help="Base directory for output files created by this script.\n",
 )
 parser.add_argument(
     "--fit_config",
@@ -704,7 +664,7 @@ client.checkConnection()
 experiment_type = ExperimentType.LUMI
 
 if experiment_type == ExperimentType.LUMI:
-    track_file_pattern = "Lumi_TrkQA_"
+    track_file_pattern = "Lumi_TrksQA_"
 elif experiment_type == ExperimentType.KOALA:
     track_file_pattern = "Koala_Track_"
 
---
+++ python/lumifit/scenario.py
@@ -0,0 +1,46 @@
+"""
+holds info about which scenario (LMD or KOALA) is run.
+"""
+
+import math
+from alignment import AlignmentParameters
+from enum import Enum
+
+
+class ExperimentType(Enum):
+    LUMI = "LUMI"
+    KOALA = "KOALA"
+
+
+class Scenario:
+    def __init__(self, dir_path_, experiment_type: ExperimentType):
+        self.momentum = 0.0
+
+        self.dir_path = dir_path_
+        self.filtered_dir_path = ""
+        self.acc_and_res_dir_path = ""
+        self.rec_ip_info = {}
+        self.elastic_pbarp_integrated_cross_secion_in_mb = None
+        self.use_m_cut = True
+        self.use_xy_cut = True
+        self.use_ip_determination = True
+        self.Lumi = True
+        if experiment_type == ExperimentType.LUMI:
+            self.phi_min_in_rad = 0
+            self.phi_max_in_rad = 2 * math.pi
+            self.Sim = "./runLmdSimReco.py"
+            self.Reco = "./runLmdReco.py"
+        else:
+            self.phi_min_in_rad = 0.9 * math.pi
+            self.phi_max_in_rad = 1.3 * math.pi
+            self.Sim = "./runKoalaSimReco.py"
+            self.Reco = "./runKoalaReco.py"
+
+        self.alignment_parameters: AlignmentParameters = AlignmentParameters()
+
+        self.state = 1
+        self.last_state = 0
+
+        self.simulation_info_lists = []
+
+        self.is_broken = False
