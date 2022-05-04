3d3f945068e337a4c5f64361164a5f503165ad2f
Roman Klasen <r.klasen@ep1.rub.de>
2022-05-04 14:27:16 +0200

    dimensionRange needs a dict as well

--- data/LinkDef.h
+++ data/LinkDef.h
@@ -23,6 +23,7 @@
 #pragma link C++ class PndLmdAcceptance + ;
 #pragma link C++ class PndLmdMapData + ;
 #pragma link C++ class EstimatorOptions + ;
+#pragma link C++ class DimensionRange + ;
 
 #pragma link C++ class std::vector < PndLmdAngularData> + ;
 #pragma link C++ class std::vector < PndLmdAcceptance> + ;
