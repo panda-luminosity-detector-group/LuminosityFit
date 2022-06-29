a4d4aed90e76b77add977d8a33ccbd28b3fbd14c
JFrech <jfrech@ep1.ruhr-uni-bochum.de>
2022-06-29 15:12:05 +0200

    WIP runKoaReco.py updated untested

--- python/runKoaReco.py
+++ python/runKoaReco.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+
 import os
 
 from lumifit.alignment import AlignmentParameters
 from lumifit.general import load_params_from_file, check_stage_success
 from lumifit.reconstruction import ReconstructionParameters
+from lumifit.general import toCbool
 
 # fixed for this installation
 lmd_build_path = os.environ["LMDFIT_BUILD_PATH"]
@@ -61,12 +63,34 @@ prefilter = False
 if reco_params.use_xy_cut:
     prefilter = True
 
+# I'm not sure target path exists yet, but we need it to copy the koa params file to
+if not os.path.isdir(workpathname):
+    os.makedirs(workpathname)
+
+
+# we always need the Koala_(Params|MC) file, no matter what (except if it already exists)
+if not check_stage_success(f"{workpathname}/Koala_Params_{start_evt}.root"):
+    os.system(
+        f"cp {path_mc_data}/Koala_Params_{start_evt}.root {workpathname}/Koala_Params_{start_evt}.root "
+    )
+    os.system(
+        f"cp {path_mc_data}/Koala_MC_{start_evt}.root {workpathname}/Koala_MC_{start_evt}.root "
+    )
+if not check_stage_success(f"{workpathname}/Koala_digi_{start_evt}.root"):
+    if os.path.exists(f"{path_mc_data}/Koala_digi_{start_evt}.root"):
+        # copy the Lumi_Digi data from permanent storage, it's needed for IP cut for the LumiFit
+        os.system(
+            f"cp {path_mc_data}/Koala_digi_{start_evt}.root {workpathname}/Koala_digi_{start_evt}.root"
+        )
+
 os.chdir(macropath)
+
 # check_stage_success "workpathname + "/Koala_MC_${start_evt}.root""
 if (
     not check_stage_success(pathname + f"/Koala_Track_{start_evt}.root")
     or force_level == 1
 ):
+    os.chdir(macropath)
     os.system(
         f"root -l -b -q 'KoaPixel2Reco.C({reco_params.num_events_per_sample},"
         + f"{start_evt},"
