915a6cac0df1897f8b56ce2646c3861b5f9a9bd0
JFrech <jfrech@ep1.ruhr-uni-bochum.de>
2022-08-10 19:57:33 +0200

    runKoaSimReco.sh now obsolete

--- python/runKoaSimReco.sh
+++ python/runKoaSimReco.sh
@@ -5,7 +5,6 @@ echo ====   DEPRECATED. DO NOT USE ANYMORE              ====
 echo ====   KEEP ONLY FOR REFERENCE, OR DELETE          ====
 echo =======================================================
 
-
 exit 1
 
 
