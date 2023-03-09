9a50174ebbd28567f7126efdecca7e66e63cb44f
Jinxin LI <30660697+JinxinLee@users.noreply.github.com>
2023-03-09 13:51:35 +0100

    fix GSL Error: change qng to qag integration
    changing qng non-adaptive Gauss-Kronrod integration method to QAG adaptive integration method, which fix the GSL error for calculating cross section with large theta angle and relatively high precision.

--- model_framework/operators1d/integration/IntegralStrategyGSL1D.cxx
+++ model_framework/operators1d/integration/IntegralStrategyGSL1D.cxx
@@ -27,10 +27,16 @@ mydouble IntegralStrategyGSL1D::Integral(Model1D *model1d, mydouble xlow,
   gsl_function F;
   F.function = &gsl_func_wrapper;
   F.params = 0;
-  if (gsl_integration_qng(&F, (double)xlow, (double)xhigh, (double)precision,
-                          (double)precision, &result, &error, &neval)) {
+  size_t limit = 1000;
+  gsl_integration_workspace* w = gsl_integration_workspace_alloc(1000);
+  
+  //if (gsl_integration_qng(&F, (double)xlow, (double)xhigh, (double)precision,
+  //                        (double)precision, &result, &error, &neval)) {
+  if (gsl_integration_qag(&F, (double)xlow, (double)xhigh, (double)precision,
+                            (double)precision,(size_t)limit, 6, w, &result, &error)) {
     // in principle we could do something if we are not precise enough
     // default will be just to return the estimate.
   }
+  gsl_integration_workspace_free(w);
   return result;
 }
