25bb8c17aa00b3ad0a7bd65ea2da2c878fb8657c
Roman Klasen <r.klasen@ep1.rub.de>
2023-09-06 15:31:27 +0200

    moved plot macros

---
+++ tools/plots/plotEfficiency.C
@@ -0,0 +1,20 @@
+#include "TCanvas.h"
+#include <string>
+
+void plotEfficiency(std::string inFileName, std::string outName) {
+  TFile *f = new TFile(inFileName.c_str());
+  auto acceptance = f->Get<PndLmdAcceptance>("lmd_acceptance");
+  auto eff2D = acceptance->getAcceptance2D();
+  {
+
+    auto canvas = TCanvas("canvas", "canvas", 1920, 1080);
+    eff2D->Draw("lego2");
+    canvas.Print((outName + "-lego2.png").c_str());
+  }
+  {
+
+    auto canvas = TCanvas("canvas", "canvas", 1080, 1080);
+    eff2D->Draw("colz");
+    canvas.Print((outName + "-colz.png").c_str());
+  }
+}
