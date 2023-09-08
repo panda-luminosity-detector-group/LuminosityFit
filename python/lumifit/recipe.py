e30f6a9e8dd87263a5e829b28532e2805ebca644
Roman Klasen <r.klasen@ep1.rub.de>
2023-09-08 13:44:06 +0200

    oh yeah, class members vs instance members. oh python...

--- python/lumifit/recipe.py
+++ python/lumifit/recipe.py
@@ -8,7 +8,7 @@ or read from file!
 from enum import Enum, IntEnum
 from typing import List
 
-from attrs import define
+from attrs import define, field
 
 
 class SimulationState(IntEnum):
@@ -50,4 +50,9 @@ class SimRecipe:
     It is never to be written to disk, it only resides in memory for the determineLuminosity script.
     """
 
-    SimulationTasks: List[SimulationTask] = []
+    SimulationTasks: List[SimulationTask] = field(init=False)
+
+    # use a post init hook to initialize the list so that it becomes a
+    # instance-specific list and not a class-specific list (thread safety!)
+    def __attrs_post_init__(self):
+        self.SimulationTasks = []
