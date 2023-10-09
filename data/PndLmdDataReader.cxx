b033114f34d91db9e767068ab88ac14989b3f2f3
Roman Klasen <r.klasen@ep1.rub.de>
2023-10-09 14:53:01 +0200

    removed the rest of the hacky halo cut

--- data/PndLmdDataReader.cxx
+++ data/PndLmdDataReader.cxx
@@ -251,15 +251,6 @@ void PndLmdDataReader::fillData(const Lmd::Data::TrackPairInfo &track_info) {
       if (!track_accepted)
         continue;
     }
-    if (track_accepted) {
-      double pX = track_info.MCIP.Momentum[0];
-      double pY = track_info.MCIP.Momentum[1];
-      double pZ = track_info.MCIP.Momentum[2];
-      double thetaX = pX / pZ;
-      double thetaY = pY / pZ;
-
-      double thetaPlane = sqrt(thetaX * thetaX + thetaY * thetaY);
-    }
     // skip tracks that do not pass the filters
     if (successfullyPassedFilters(registered_acceptances[i], track_info)) {
       if (registered_acceptances[i]->getSecondaryDimension().is_active) {
