3d893e167dbe6c4a42c2c0bf7a4d76cbf606eef5
Roman Klasen <r.klasen@ep1.rub.de>
2023-10-12 11:10:12 +0200

    Hmmm, the fixing part doesn't work

--- ui/PndLmdFitFacade.cxx
+++ ui/PndLmdFitFacade.cxx
@@ -270,14 +270,14 @@ void PndLmdFitFacade::initIPForModel(std::shared_ptr<Model> current_model,
       .getModelParameter("offset_y")
       ->setValue(model_opt_ptree.get<double>("ip_offset_y"));
 
-  if (model_opt_ptree.get<bool>("fix_ip_offsets")) {
-    current_model->getModelParameterSet()
-        .getModelParameter("offset_x")
-        ->setParameterFixed(true);
-    current_model->getModelParameterSet()
-        .getModelParameter("offset_y")
-        ->setParameterFixed(true);
-  }
+  // if (model_opt_ptree.get<bool>("fix_ip_offsets")) {
+  current_model->getModelParameterSet()
+      .getModelParameter("offset_x")
+      ->setParameterFixed(true);
+  current_model->getModelParameterSet()
+      .getModelParameter("offset_y")
+      ->setParameterFixed(true);
+  // }
 }
 
 void PndLmdFitFacade::initBeamParametersForModel(
