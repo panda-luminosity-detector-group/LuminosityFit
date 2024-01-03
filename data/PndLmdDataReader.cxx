127d27460f02385fe97c7c4f4749e07037b7808d
Roman Klasen <r.klasen@ep1.rub.de>
2024-01-03 12:46:37 +0100

    removed halo cut and skip secondary tracks during res/acc runs

--- data/PndLmdDataReader.cxx
+++ data/PndLmdDataReader.cxx
@@ -248,32 +248,8 @@ void PndLmdDataReader::fillData(const Lmd::Data::TrackPairInfo &track_info) {
   for (unsigned int i = 0; i < registered_acceptances.size(); i++) {
     bool track_accepted = wasReconstructed(track_info);
     if (track_info.IsSecondary) {
-      //if (!track_accepted)
         continue;
     }
-    /*
-    if (track_accepted) {
-      double pX = track_info.MCIP.Momentum[0];
-      double pY = track_info.MCIP.Momentum[1];
-      double pZ = track_info.MCIP.Momentum[2];
-      double thetaX = pX / pZ;
-      double thetaY = pY / pZ;
-
-      double thetaPlane = sqrt(thetaX * thetaX + thetaY * thetaY);
-
-      //* --------------------
-      //* This is the halo cut
-      //* --------------------
-      // FIXME: this doesn't belong here, but more appropriately in the track
-      // reconstruction that is in PandaRoot however, not Lumi Fit
-      // if (thetaPlane < 0.003 || thetaPlane > 0.0105) {
-      if (thetaPlane < 0.003) {
-        track_accepted = false;
-        std::cout << "skipping track with thetaPlane = " << thetaPlane
-                  << std::endl;
-      }
-    }
-    */
     // skip tracks that do not pass the filters
     if (successfullyPassedFilters(registered_acceptances[i], track_info)) {
       if (registered_acceptances[i]->getSecondaryDimension().is_active) {
